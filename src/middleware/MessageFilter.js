0fa2aaa9a6520426ec7e819d78331ba63abce264
viethoang1520 <vhd151020@gmail.com>
2024-09-17 14:32:54 +0700

    fix messageFilter middleware

--- src/middleware/MessageFilter.js
+++ src/middleware/MessageFilter.js
@@ -1,8 +1,13 @@
 const Filter = require('bad-words')
-const filter = new Filter()
-filter.addWords('cặc', 'lồn', 'vú', 'dú', 'đít', 'địt', 'sex', 'dâm', 'chim', 'x', 'lưỡi', 'qh', 'răm', 'zam')
+const filter = []
+filter.push('cặc', 'lồn', 'vú', 'dú', 'đít', 'địt', 'sex', 'dâm', 'chim', 'x', 'lưỡi', 'qh', 'răm', 'zam', 'sét', 'bú')
 
 const filterMessage = (message) => {
-  return cleanMessage =  filter.clean(message)
+  const words = message.split(/\s+/);
+
+  const cleanedMessage = words.map((word) => {
+    return filter.includes(word.toLowerCase()) ? '***' : word;
+  });
+  return cleanedMessage.join(' ');
 }
 module.exports = filterMessage
